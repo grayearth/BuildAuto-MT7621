a86c08fb13dfced3fa8329e0534f558b618a11d8
grayearth <108775463+grayearth@users.noreply.github.com>
2022-12-01 22:30:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改默认IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.80.1/g' package/base-files/files/bin/config_generate
 
 # 补充汉化       
 cp -f ./feeds/springwrt/files/udpxy.lua ./feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi
